561ba636bad81cc081e2d034b6c98ce123f9ebbb
therealOri <zxcft96@gmail.com>
2026-01-30 11:10:48 -0700

    Fixed typos: shortner -> shortener

--- services/__init__.py
+++ services/__init__.py
@@ -2,6 +2,6 @@ from .ip_lookup import *
 from .vpn_proxy import *
 from .legitimate import *
 from .abuse import *
-from .url_shortner import *
+from .url_shortener import *
 from .tor_lookup import *
 from .mail_checker import *
